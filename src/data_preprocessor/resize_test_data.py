4ad6582b364e905a093c7e1aca481ae2b5547884
Khanh Bùi <54497316+khuongdz@users.noreply.github.com>
2022-08-15 18:22:03 +0700

    new file resize_test_data.py

---
+++ src/data_preprocessor/resize_test_data.py
@@ -0,0 +1,29 @@
+import setting.cfg as cfg  # cfg file to get paths
+from npy_append_array import NpyAppendArray as npy_file
+import cv2
+import os
+from glob import glob
+import numpy as np
+
+
+cfg.remove_file(cfg.resize_test_data_out)
+cfg.remove_file(cfg.test_data_framename)
+
+
+def read_img(path):
+
+    return cv2.resize(cv2.imread(path), [224, 224])
+
+
+names = []
+
+with npy_file(cfg.resize_test_data_out) as frames:
+    for vid_name in glob(cfg.test_data_path+"*"):
+        print(vid_name)
+        for frame_name in glob(os.path.join(vid_name, "*")):
+            frames.append(np.expand_dims(read_img(frame_name), axis=0))
+            names.append(
+                f'{os.path.basename(vid_name)}_{os.path.basename(frame_name[:-4:])}')
+
+
+np.save(cfg.test_data_framename,names)
