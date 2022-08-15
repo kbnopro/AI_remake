dceec27f9eb8896e5afc9bb6fe8cfd4c8960c0b4
Khanh Bùi <54497316+khuongdz@users.noreply.github.com>
2022-08-15 17:27:42 +0700

    continue building cfg.py

--- src/data_preprocessor/setting/cfg.py
+++ src/data_preprocessor/setting/cfg.py
@@ -9,3 +9,17 @@ def get_wsp_path () -> str :
 wsp_path = get_wsp_path()
 src_path = os.path.join(wsp_path,"src")
 
+test_data_path = os.path.join(src_path,"raw_dataset","Test_data","")
+train_data_path = os.path.join(src_path,"raw_datset","train_data","")
+
+preprocessed_data_path = os.path.join(src_path,"preprocessed_data","")
+print(preprocessed_data_path)
+
+
+resize_test_data_out = os.path.join(preprocessed_data_path,"resize_test_data_out.npy")
+test_data_framename = os.path.join(preprocessed_data_path,"test_data_framename.npy")
+
+resize_train_data_out = os.path.join(preprocessed_data_path,"resize_train_data_out.npy")
+train_data_label = os.path.join(preprocessed_data_path,"train_data_label")
+
+
