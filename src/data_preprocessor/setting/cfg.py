9786c6e8a3f493e3db296ac9cbebe00ceeacba40
Khanh Bùi <54497316+khuongdz@users.noreply.github.com>
2022-08-15 17:31:31 +0700

    continue building cfg.py

--- src/data_preprocessor/setting/cfg.py
+++ src/data_preprocessor/setting/cfg.py
@@ -6,6 +6,11 @@ def get_wsp_path () -> str :
         wsp_path = os.path.dirname(wsp_path)
     return wsp_path
 
+def remove_file ( path ) -> None:
+    path : str 
+    if os.path.exists(path) :
+        os.remove(path)
+
 wsp_path = get_wsp_path()
 src_path = os.path.join(wsp_path,"src")
 
